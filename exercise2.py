8760c65f6a6ebcb441143de7a234318b7c1fbdb9
adva15 <aadva5319@gmail.com>
2024-09-05 21:55:45 +0300

    Add files via upload

---
+++ exercise2.py
@@ -0,0 +1,19 @@
+# start
+
+test_score:int =int(input("test_score:"))
+
+if  0 <=  test_score <= 40:
+      print("try harder next time...")
+elif  41 <=  test_score <= 60:
+        print("you're getting there, need some more work")
+elif 61 <= test_score <= 80:
+    print("good pretty")
+elif 81 <=  test_score <= 95:
+    print("!awesome")
+elif 96 <= test_score <= 100:
+    print("excellent!!! You're a Star!")
+
+else:
+    print("grade illegal")
+
+# end
