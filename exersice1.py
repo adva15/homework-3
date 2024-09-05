a7826e92a70c78abbedc3b00f14c62909304cacd
adva15 <aadva5319@gmail.com>
2024-09-05 22:06:44 +0300

    Add files via upload

---
+++ exersice1.py
@@ -0,0 +1,19 @@
+# start
+
+id: str = str(input("id:"))
+name: str = str(input("name:"))
+last_name: str = str(input("last_name:"))
+height: float = float(input("height:"))
+year_of_birth: int = int(input("year_of_birth:"))
+
+print(F"id:{id},name:{name},last_name:{last_name},height:{height},year_of_birth:{year_of_birth}")
+
+name1: str = "eden golan"
+name2: str = "orit superman"
+name3: str = "sami bukobza"
+
+print(f"name: {name1:>13}")
+print(f"name: {name2:^13}")
+print(f"name: {name3:>13}")
+
+# end
